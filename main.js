164c33448a576b294813a462f59f71ed8fd08590
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2021-05-28 15:51:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-current=new Date();
-x="";
-setInterval(function(){
-        x=current.getSeconds()+"sec/"+current.getMinutes()+"min/"+current.getHours()+"hrs";
-        console.log(x);
+song_1="";
+song_2="";
+function preload(){
+    song_1=loadSound("");
+    song_2=loadSound("");
 }
-,1000);
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,600,500)
+}
+function song1(){
+    song_1.play();
+}
+function song2(){
+    song.play();
+}
